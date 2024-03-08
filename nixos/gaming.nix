fe04d9066df99ab90fb12afb8d3bf5d646d13c2d
Corey Petty <corey@status.im>
2024-03-08 15:42:39 -0500

    added gaming back in

---
+++ nixos/gaming.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  # Enable 32bit OpenGL and PulseAudio
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.pulseaudio.support32Bit = true;
+
+  # XBox Pad kernel driver.
+  hardware.xone.enable = false;
+  hardware.xpadneo.enable = true;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Packages required for work
+  users.users.petty.packages = with pkgs; [
+    steam
+    lutris
+    corefonts
+    vulkan-tools
+    # Wine
+    wineWowPackages.stableFull
+    winetricks
+  ];
+}
--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -4,7 +4,7 @@
   hardware.opengl.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    package =  pkgs.linuxKernel.packages.linux_lqx.nvidia_x11;
+    package =  pkgs.linuxKernel.packages.linux_zen.nvidia_x11;
     modesetting.enable = true;
     nvidiaSettings = true;
     forceFullCompositionPipeline = true;
